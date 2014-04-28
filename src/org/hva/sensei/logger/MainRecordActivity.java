73b3de5b4255a55a8ea197e90e8a85ce19cb3a5b
Joey van der Bie <joeyvanderbie@gmail.com>
2014-04-28 18:01:47 +0200

    first attempt to frequency trigger

--- src/org/hva/sensei/logger/MainRecordActivity.java
+++ src/org/hva/sensei/logger/MainRecordActivity.java
@@ -3,9 +3,6 @@ package org.hva.sensei.logger;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 import java.util.Timer;
@@ -33,20 +30,24 @@ import android.widget.ToggleButton;
 
 public class MainRecordActivity extends Activity {
 	private AudioRecorder mRecorder = null;
-	private Timer mUiTimer = null;
+	private Timer recordingTimer = null;
+	private Timer frequencyTimer = null;
 	private long mRecordingStartTime = 0;
+	private long mRecordingFrequencyStartTime = 0;
 	private String mLastFileName = "";
 	private MediaPlayer mMediaPlayer = null;
 	String PATH_TO_FILES = Environment.getExternalStorageDirectory()
 			+ "/Sensei/Recording/";
+	String fileExt = ".wav";
 	private ToggleButton record_button;
 	private TextView recording_duration_view;
 	private TextView recording_frequency_view;
+	private TextView recording_duration_icon;
 
 	private int recording_duration = 30;
 	private int recording_frequency = 300;
 
-	class UiUpdateTask extends TimerTask {
+	class RecordTask extends TimerTask {
 		public void run() {
 			final VuMeterView vuMeter = (VuMeterView) findViewById(R.id.vuMeterView);
 			final TextView durationText = (TextView) findViewById(R.id.recording_timer_info);
@@ -73,6 +74,37 @@ public class MainRecordActivity extends Activity {
 				runOnUiThread(new Runnable() {
 					public void run() {
 						onClickRecord(false);
+						startRecordingWithFrequency(true);
+					}
+				});
+
+			}
+
+		}
+	}
+
+	class RecordFrequencyTask extends TimerTask {
+		public void run() {
+			final TextView durationText = (TextView) findViewById(R.id.recording_timer_info);
+			final long duration = (recording_frequency * 1000)
+					- (System.currentTimeMillis() - mRecordingFrequencyStartTime);
+
+			runOnUiThread(new Runnable() {
+				public void run() {
+					durationText.setText(String.format(
+							"%02d:%02d",
+							TimeUnit.MILLISECONDS.toMinutes(duration),
+							TimeUnit.MILLISECONDS.toSeconds(duration)
+									- TimeUnit.MINUTES
+											.toSeconds(TimeUnit.MILLISECONDS
+													.toMinutes(duration))));
+				}
+			});
+
+			if (duration <= 0) {
+				runOnUiThread(new Runnable() {
+					public void run() {
+						onClickRecord(true);
 					}
 				});
 
@@ -95,7 +127,8 @@ public class MainRecordActivity extends Activity {
 						.getText().toString());
 				recording_frequency = Integer.parseInt(recording_frequency_view
 						.getText().toString());
-				onClickRecord(((ToggleButton) v).isChecked());
+				//onClickRecord(((ToggleButton) v).isChecked());
+				startRecordingWithFrequency(((ToggleButton) v).isChecked());
 			}
 		});
 
@@ -103,6 +136,7 @@ public class MainRecordActivity extends Activity {
 		recording_duration_view.setText("" + recording_duration);
 		recording_frequency_view = (TextView) findViewById(R.id.recording_frequency);
 		recording_frequency_view.setText("" + recording_frequency);
+		recording_duration_icon = (TextView) findViewById(R.id.recording_timer_info_icon);
 
 	}
 
@@ -139,18 +173,44 @@ public class MainRecordActivity extends Activity {
 		}
 	}
 
-	private void onClickRecord(boolean start) {
-		record_button.setChecked(start);
+	private void startRecordingWithFrequency(boolean start) {
 		if (record_button.isChecked()) {
+			mRecordingFrequencyStartTime = System.currentTimeMillis();
+
 			// force disabled before recording
 			record_button.setChecked(false);
 			record_button.setEnabled(false);
+			//
+			frequencyTimer = new Timer();
+			frequencyTimer.schedule(new RecordFrequencyTask(), 10, 50);
+		} else {
+			record_button.setChecked(true);
+			record_button.setEnabled(false);
+
+			// stop recording
+			onClickRecord(false);
+
+			// stop frequency trigger
+			frequencyTimer.cancel();
+
+			record_button.setChecked(false);
+			record_button.setEnabled(true);
+		}
+	}
+
+	private void onClickRecord(boolean start) {
+//		record_button.setChecked(start);
+		//if (record_button.isChecked()) {
+		if(start){
+			// force disabled before recording
+//			record_button.setChecked(false);
+//			record_button.setEnabled(false);
 
 			// prepare recording
 			SimpleDateFormat fileNameDate = new SimpleDateFormat();
 			fileNameDate.applyPattern("yyyy-MM-dd HH.mm.ss");
 			// mLastFileName = fileNameDate.format(new Date()) + ".3gp";
-			mLastFileName = fileNameDate.format(new Date()) + ".wav";
+			mLastFileName = fileNameDate.format(new Date()) + fileExt;
 			mRecorder = new AudioRecorder(PATH_TO_FILES + mLastFileName);
 
 			// start recording
@@ -158,23 +218,25 @@ public class MainRecordActivity extends Activity {
 				mRecorder.start();
 			} catch (IOException e) {
 				makeAlert("Error while starting recording:\n" + e.getMessage());
-				record_button.setChecked(false);
-				record_button.setEnabled(true);
+//				record_button.setChecked(false);
+//				record_button.setEnabled(true);
 			} finally {
-				record_button.setChecked(true);
-				record_button.setEnabled(true);
+//				record_button.setChecked(true);
+//				record_button.setEnabled(true);
 
-				mUiTimer = new Timer();
-				mUiTimer.schedule(new UiUpdateTask(), 10, 50);
+				recording_duration_icon.setVisibility(View.VISIBLE);
+				
+				recordingTimer = new Timer();
+				recordingTimer.schedule(new RecordTask(), 10, 50);
 
 				mRecordingStartTime = System.currentTimeMillis();
 			}
 		} else {
-			record_button.setChecked(true);
-			record_button.setEnabled(false);
+//			record_button.setChecked(true);
+//			record_button.setEnabled(false);
 
 			try {
-				mUiTimer.cancel();
+				recordingTimer.cancel();
 				mRecorder.stop();
 
 				// ToggleButton playBtn = (ToggleButton)
@@ -183,31 +245,33 @@ public class MainRecordActivity extends Activity {
 				// playBtn.setAlpha(1f);
 			} catch (IOException e) {
 				makeAlert("Error while stopping recording:\n" + e.getMessage());
-				record_button.setChecked(true);
-				record_button.setEnabled(true);
+//				record_button.setChecked(true);
+//				record_button.setEnabled(true);
 			} finally {
-				record_button.setChecked(false);
-				record_button.setEnabled(true);
+//				record_button.setChecked(false);
+//				record_button.setEnabled(true);
+
+				recording_duration_icon.setVisibility(View.GONE);
 				updateFileList();
 			}
 		}
 	}
-	
+
 	private void updateFileList() {
 		File dir = new File(PATH_TO_FILES);
-		List<String> list = Util.getSortedFilenames(dir, ".wav");
+		List<String> list = Util.getSortedFilenames(dir, fileExt);
 		LinearLayout files = (LinearLayout) findViewById(R.id.fileList);
 		files.removeAllViews();
 		for (final String file : list) {
 			Button b = new Button(this);
-			b.setText(this.getString(R.string.share_prefix_button) + " "+file);
+			b.setText(this.getString(R.string.share_prefix_button) + " " + file);
 			b.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View v) {
 					Intent sharingIntent = new Intent(
 							android.content.Intent.ACTION_SEND);
-		            sharingIntent.setType("audio/wav");
+					sharingIntent.setType("audio/wav");
 					sharingIntent.putExtra(
 							android.content.Intent.EXTRA_SUBJECT,
 							getResources().getString(R.string.share_title));
